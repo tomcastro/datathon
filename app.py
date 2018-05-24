1cac1ffc3f44e09c6e760896dcbd9c681e4e48dd
tcastrog10 <tcastrog10@gmail.com>
2018-05-24 16:55:57 -0400

    env check

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
-# Get dataset 2, rename last column, group by category and year
+# Get CSV files depending on environment
 
-current_path = os.getcwd()
-exp_path = os.path.join(current_path, 'datasets', 'dataset_2.csv')
+if 'ENV' in os.environ and os.environ['ENV'] == 'dev':
+    current_path = os.getcwd()
+    exp_path = os.path.join(current_path, 'datasets', 'dataset_2.csv')
+    pop_path = os.path.join(current_path, 'datasets', 'dataset_4.csv')
+else:
+    base_url = 'http://s3.amazonaws.com/uai-datathon-datasets/datasets/'
+    exp_path = base_url + 'dataset_2.csv'
+    pop_path = base_url + 'dataset_4.csv'
+
+# Get dataset 2, rename last column, group by category and year
 
 df_gastos = pd.read_csv(exp_path, sep=';')
 cols = df_gastos.columns.values
@@ -24,8 +32,6 @@ df_gastos = df_gb.reset_index()
 CATEGORIES = df_gastos['Partida'].unique()
 
 # dataset 4
-
-pop_path = os.path.join(current_path, 'datasets', 'dataset_4.csv')
 
 df_pop = pd.read_csv(pop_path, sep=';')
 cols = df_pop.columns.values
@@ -57,7 +63,9 @@ app.layout = html.Div(children=[
 
     dcc.Dropdown(
         id='categories',
-        options=[{'label': i, 'value': i} for i in df_gastos['Partida'].unique()],
+        options=[{
+            'label': i, 'value': i
+        } for i in df_gastos['Partida'].unique()],
         multi=True
     ),
 
